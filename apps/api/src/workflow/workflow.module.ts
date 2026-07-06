ab2bd2ecb760e0d25657d1ecf036d02393337c34
usmankhan4001 <ranausmankhan4001@gmail.com>
2026-07-06 13:44:08 +0500

    Add CompatibilityController to handle legacy Bitrix24 webhook URL paths

--- apps/api/src/workflow/workflow.module.ts
+++ apps/api/src/workflow/workflow.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { WorkflowController } from './workflow.controller';
+import { CompatibilityController } from './compatibility.controller';
 import { WorkflowService } from './workflow.service';
 import { WhatsappModule } from '../whatsapp/whatsapp.module';
 
 @Module({
   imports: [WhatsappModule],
-  controllers: [WorkflowController],
+  controllers: [WorkflowController, CompatibilityController],
   providers: [WorkflowService],
   exports: [WorkflowService], // exported so CronModule can inject it
 })
 export class WorkflowModule {}
+
